a2a2f431666e3c6f720b85fb368d0f9746b57b52
Nguyen Dung <dungntp@evolableasia.vn>
2018-01-09 16:54:41 +0700

    Update Login and connect Db code

--- TestASPNETIdentity/TestASPNETIdentity/App_Start/Startup.cs
+++ TestASPNETIdentity/TestASPNETIdentity/App_Start/Startup.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Owin; 
+using Owin;
 
 namespace TestASPNETIdentity.App_Start
 {
---
+++ TestASPNETIdentity/TestASPNETIdentity/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Claims;
+
+using TestASPNETIdentity.Models;
+
+namespace TestASPNETIdentity.Controllers
+{
+    [AllowAnonymous]
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        [HttpGet]
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Users model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            if ((model.Email == "admin@admin.com") && (model.Password == "12345"))
+            {
+                var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, "Yu"),
+                    new Claim(ClaimTypes.Email, "yuki@email.com"),
+                    new Claim(ClaimTypes.Country, "VN"),
+                }, "ApplicationCookie");
+                var ctx = Request.GetOwinContext();
+                var authManager = ctx.Authentication;
+                authManager.SignIn(identity);
+            }
+            ModelState.AddModelError("", "Invalid email or password");
+            return View(model);
+        }
+        public ActionResult Logout()
+        {
+            var ctx = Request.GetOwinContext();
+            var authManager = ctx.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+            return RedirectToAction("Login", "Auth");
+        }
+
+        public ActionResult Registraion()
+        {
+            return View();
+        }
+    }
+}
